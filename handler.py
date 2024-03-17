4ff415f2a1830b88c1e64a7effe430f46001f244
CodeDann <sc20jdpn@leeds.ac.uk>
2024-03-17 16:44:22 +0000

    first db connect file

--- handler.py
+++ handler.py
@@ -2,29 +2,7 @@ import json
 import requests
 
 
-def get_val_or_error(request, key):
-    val = request.get(key)
-    if val is None or val == "":
-        raise ValueError(f"Missing required parameter '{key}'")
-
-
 def handle(req):
-    try:
-        data = json.loads(req)
-        latitude = get_val_or_error(data, 'lat')
-        longitude = get_val_or_error(data, 'long')
-    except ValueError as e:
-        print(str(e))
-    except Exception as e:
-        print(str(e))
-
-    # # todo store API more securely
-    api_key = "044d96d0a9150903ca5e80fc1a5da8e7"
-
-    try:
-        url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}"
-        response = requests.get(url)
-        data = response.json()    
-        print(json.dumps(data))
-    except Exception as e:
-        print(str(e))
+    data = json.loads(req)
+    print("YOYO gotta connect to database")
+    print(data)
