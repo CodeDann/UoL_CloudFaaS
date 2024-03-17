e16a9b629897697212888eabe9cb4108a46d04cf
CodeDann <sc20jdpn@leeds.ac.uk>
2024-03-17 16:34:34 +0000

    added error handling

--- handler.py
+++ handler.py
@@ -9,18 +9,15 @@ def get_val_or_error(request, key):
 
 
 def handle(req):
-    # print(req)
-    # try:
-    #     latitude = get_val_or_error(req, 'lat')
-    #     longitude = get_val_or_error(req, 'long')
-    # except ValueError as e:
-    #     return str(e)
-    data = json.loads(req)
-    latitude = data.get('lat')
-    longitude = data.get('long')
-        # print(str(e))
-        # return func.HttpResponse(str(e), status_code=400)
-    # print(latitude, longitude)
+    try:
+        data = json.loads(req)
+        latitude = get_val_or_error(data, 'lat')
+        longitude = get_val_or_error(data, 'long')
+    except ValueError as e:
+        print(str(e))
+    except Exception as e:
+        print(str(e))
+
     # # todo store API more securely
     api_key = "044d96d0a9150903ca5e80fc1a5da8e7"
 
@@ -31,44 +28,3 @@ def handle(req):
         print(json.dumps(data))
     except Exception as e:
         print(str(e))
-    #     return func.HttpResponse(str(e), status_code=500)
-
-    # return func.HttpResponse(json.dumps(data), status_code=200)
-        
-    
-
-
-# def handle(req):
-#     print(req)
-    # result = {"result": ""}
-
-    # json_req = json.loads(req)
-
-    # lat = requests.get(json_req["lat"])
-    # if json_req["lat"] not in lat.text:
-    #     result = {"result": "Error: latitude not found in request"}
-
-    # long = requests.get(json_req["long"])
-    # if json_req["long"] not in long.text:
-    #     result = {"result": "Error: longitude not found in request"}
-
-    # api_key = "044d96d0a9150903ca5e80fc1a5da8e7"
-
-
-    # if result["result"] == "":
-    #     try:
-    #         url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={long}&appid={api_key}"
-    #         response = requests.get(url)
-    #         data = response.json()
-    #         result = {"result": data}    
-    #     except Exception as e:
-    #         result = {"result": str(e)}
-    
-    # print(json.dumps(result))
-
-
-
-
-
-
-# print(handle('{"lat": 51.5074, "long": 0.1278}')
