b2080168289316be310c4fa63637d1b5d87ac955
CodeDann <sc20jdpn@leeds.ac.uk>
2024-03-18 15:40:37 +0000

    wip

--- handler.py
+++ handler.py
@@ -27,118 +27,13 @@ def handle(req):
             "status": 500,
             "message": "Failed to connect to the database"
         }
-
+    
+    # parse the request
     try:
-        data = json.loads(req)
-        if len(data) == 0:
-            raise ValueError("No data provided")
-        elif len(data) == 1:
-            city = get_val_or_error(data, "city")
-            coords = get_coords_from_db(cursor, city)
-        elif len(data) == 3:
-            city = get_val_or_error(data, "city")
-            latitude = get_val_or_error(data, "lat")
-            longitude = get_val_or_error(data, "lon")
-            # check if the city exists in the database
-            coords = get_coords_from_db(cursor, city)
-            if coords is None:
-                # if not, add it
-                add_city_to_db(cursor, cnx, city, latitude, longitude)
-            else:
-                # if it does, update the coordinates
-                update_city_in_db(cursor, cnx, city, latitude, longitude)
+        req = json.loads(req)
+        city = get_val_or_error(req, "city")
     except ValueError as e:
         return {
             "status": 400,
             "message": str(e)
         }
-
-    if coords is not None:
-        output = call_api(coords[0], coords[1], city)
-        update_averages(output)
-    else:
-        return {
-            "status": 404,
-            "message": "City not found",
-            "message-long": "If you want to add it to the database, please provide the latitude and longitude."
-        }
-   
-
-def get_coords_from_db(cursor, city):
-    try:
-        query = f"SELECT latitude, longitude FROM cities WHERE city_name = '{city}'"
-        cursor.execute(query)
-        row = cursor.fetchone()
-        if row is not None:
-            latitude = row[0]
-            longitude = row[1]
-            return [latitude, longitude]
-        else:
-            return None
-    except Exception as e:
-        print(str(e))
-        return None
-
-def call_api(latitude, longitude, city):
-    api_key = "044d96d0a9150903ca5e80fc1a5da8e7"
-
-    try:
-        url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}"
-        response = requests.get(url)
-        data = response.json()   
-        output = format_data(data, city) 
-        return output
-    except Exception as e:
-        print(str(e))
-
-
-def format_data(data, city):
-    try:
-        wind = data["wind"]["speed"]
-        temp = data["main"]["temp"]
-        temp_min = data["main"]["temp_min"]
-        temp_max = data["main"]["temp_max"]
-        pressure = data["main"]["pressure"]
-        humidity = data["main"]["humidity"]
-        output_data = {
-            "city": city,
-            "wind": wind,
-            "temp": temp,
-            "temp_min": temp_min,
-            "temp_max": temp_max,
-            "pressure": pressure,
-            "humidity": humidity
-        }
-        return output_data
-
-    except Exception as e:
-        print(str(e))
-
-
-def add_city_to_db(cursor, cnx, city, latitude, longitude):
-    try:
-        query = f"INSERT INTO cities (city_name, latitude, longitude) VALUES ('{city}', {latitude}, {longitude})"
-        cursor.execute(query)
-        print(f"Added city '{city}' to the database")
-        cnx.commit()
-    except Exception as e:
-        print(str(e))
-
-def update_city_in_db(cursor, cnx, city, latitude, longitude):
-    try:
-        query = f"UPDATE cities SET latitude = {latitude}, longitude = {longitude} WHERE city_name = '{city}'"
-        cursor.execute(query)
-        print(f"Updated city '{city}' in the database")
-        cnx.commit()
-    except Exception as e:
-        print(str(e))
-
-
-def check_red_flags(data):
-    try:
-        response = requests.post("http://gateway.openfaas:8080/function/red-flags", json=data)
-        print(response.json())
-    except Exception as e:
-        print(str(e))
-
-handle('{"city": "London"}')
